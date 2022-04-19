c9771d167ad27dc687d9ae1c5847569e2dedfec8
Joel Stansbury <Joel.Stansbury@gtri.gatech.edu>
2022-04-19 15:24:04 -0400

    #8: updated table parser to

--- ipypdf/utils/table_extraction.py
+++ ipypdf/utils/table_extraction.py
@@ -2,20 +2,17 @@ import numpy as np
 import pandas as pd
 import pytesseract as tess
 
+from .tess_utils import im_to_data
+
 
 def grid_detect(
     im,
     v_thresh=0.05,
-    h_thresh=0.05,
-    min_v_gap=10,  # pixels
-    min_h_gap=15,  # pixels
+    h_thresh=0.1,
+    min_v_gap=0.05,  # percentage of average word height
+    min_h_gap=2,  # percentage of average word height
 ):
     """
-    This method assumes that darker pixels correspond to text within a cell. The mask
-    used to generate the cell grid is just the normalized black/white image. This method is
-    faster but more noisy than the ocr method.
-
-    :params:
     im <PIL.Image or imarray>: Image of a table
 
     v_thresh <float> (0.1): Threshold for considering a row of pixels to be considered
@@ -23,21 +20,27 @@ def grid_detect(
 
     h_thresh <float> (0.1): Threshold for considering a column of pixels to be considered
         a horizontal boundary in the grid. Higher values mean fewer columns.
-
-    :returns:
-    grid <list>: list of bboxes corresponding to predicted cells
     """
+    # use Tesseract to find all words
+    df = im_to_data(im, scaling_factor=1)
+    boxes = df[["left", "top", "width", "height"]].values
+    avg_word_height = boxes[:, 3].min()
+    min_v_gap *= avg_word_height
+    min_h_gap *= avg_word_height
+    # min_v_gap = 1
+    # min_h_gap = 1
+    # print(min_h_gap)
 
-    mask = np.array(im).sum(axis=2).T
-    mask = mask / mask.max()
-    mask -= 1
-    mask = abs(mask)
-    mask = (mask > 0.1).astype(int)
+    # create a word mask from tesseract bboxes
+    mask = np.zeros(im.size)
+    for bbox in boxes:
+        x, y, w, h = bbox
+        mask[x : x + w, y : y + h] = 1
 
     v_density = mask.sum(axis=0) / max(mask.sum(axis=0))
     h_density = mask.sum(axis=1) / max(mask.sum(axis=1))
 
-    # scale up the density vectors so that the mean is equal to 0.2
+    # scale the density vectors so that the mean is equal to 0.2
     # this makes the thresholds directly related to the average density
     v_density = v_density / (np.mean(v_density) * 4)
     h_density = h_density / (np.mean(h_density) * 4)
@@ -50,32 +53,16 @@ def grid_detect(
     for y, val in enumerate(v_density):
         if val > v_thresh and out:
             out = False
-            grid_y.append([y])
-        if not val > v_thresh and not out:
+            if len(grid_y) == 0 or y - grid_y[-1][-1] > min_v_gap:
+                grid_y.append([y])
+            else:
+                grid_y[-1] = [grid_y[-1][0]]
+        if val < v_thresh and not out:
             grid_y[-1].append(y)
             out = True
         if not out:  # inside of a peak
             if val > 1.5:
                 to_del.add(len(grid_y) - 1)
-    if len(grid_y[-1]) == 1:
-        grid_y[-1].append(im.size[1] - 1)
-
-    # remove spans which exceed the limit of being considered a row.
-    # (this is used to filter out horizontal and vertical lines)
-    for x in sorted(list(to_del), reverse=True):
-        grid_y.pop(x)
-
-    # join spans which are too close together
-    # (ocr does this in its own way, taking advantage of known character height)
-    tmp = [grid_y[0]]
-    for gy in grid_y[1:]:
-        l = tmp[-1][1]
-        r = gy[0]
-        if r - l < min_v_gap:
-            tmp[-1][1] = gy[1]
-        else:
-            tmp.append(gy)
-    grid_y = tmp
 
     # expand to fill
     grid_y[0][0] = 0
@@ -94,32 +81,16 @@ def grid_detect(
     for x, val in enumerate(h_density):
         if val > h_thresh and out:
             out = False
-            grid_x.append([x])
+            if len(grid_x) == 0 or x - grid_x[-1][-1] > min_h_gap:
+                grid_x.append([x])
+            else:
+                grid_x[-1] = [grid_x[-1][0]]
         if not val > h_thresh and not out:
             grid_x[-1].append(x)
             out = True
         if not out:  # inside of a peak
             if val > 1.5:
                 to_del.add(len(grid_x) - 1)
-    if len(grid_x[-1]) == 1:
-        grid_x[-1].append(im.size[0] - 1)
-
-    # remove spans which exceed the limit of being considered a row.
-    # (this is used to filter out horizontal and vertical lines)
-    for x in sorted(list(to_del), reverse=True):
-        grid_x.pop(x)
-
-    # join spans which are too close together
-    # (ocr does this in its own way, taking advantage of known character height)
-    tmp = [grid_x[0]]
-    for gx in grid_x[1:]:
-        b = tmp[-1][1]
-        t = gx[0]
-        if t - b < min_h_gap:
-            tmp[-1][1] = gx[1]
-        else:
-            tmp.append(gx)
-    grid_x = tmp
 
     # expand to fill
     grid_x[0][0] = 0
@@ -130,9 +101,6 @@ def grid_detect(
         m = int((x1 + x2) / 2)
         grid_x[i][1] = m
         grid_x[i + 1][0] = m
-        # diff = x2-x1
-        # grid_x[i][1]+=int(3*diff/4)
-        # grid_x[i+1][0]-=int(diff/4)
 
     grid = []
     for y in grid_y:
@@ -140,8 +108,7 @@ def grid_detect(
         for x in grid_x:
             x1, x2 = x
             grid.append([x1, y1, x2 - x1, y2 - y1])
-
-    return grid
+    return grid, df
 
 
 def contains(coords1, coords2):
@@ -164,39 +131,6 @@ def grid_2_table(grid, df=None):
     return [cells[i : i + n] for i in range(0, len(cells), n)]
 
 
-def tessdata_to_df(tessdata, keep_garbage=False):
-    """Ingests a string repr of tesseract output and spits out a dataframe"""
-    rows = [r.split("\t") for r in tessdata.split("\n")[:-1]]
-    h = rows[0]
-    rows = rows[1:]
-
-    df = pd.DataFrame(rows)
-    df.columns = h
-
-    # set types
-    dtypes = [int] * 10 + [float, str]
-    for c, t in zip(df.columns, dtypes):
-        df[c] = df[c].values.astype(t)
-
-    if not keep_garbage:
-        df = df[[x.strip() != "" for x in df["text"]]]
-        df = df[df["conf"] > 0].reset_index()
-
-    return df
-
-
-def im_to_data(im):
-    tessdata = tess.image_to_data(im, config="--psm 1")
-    return tessdata_to_df(tessdata)
-
-
 def img_2_table(im):
-    grid = grid_detect(
-        im,
-        v_thresh=0.1,
-        h_thresh=0.2,
-        min_v_gap=5,  # pixels
-        min_h_gap=10,  # pixels
-    )
-    df = im_to_data(im)
+    grid, df = grid_detect(im)
     return grid_2_table(grid, df)
