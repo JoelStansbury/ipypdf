787da3dd5407acfc985c272f55c24ffb39298d9c
Joel Stansbury <Joel.Stansbury@gtri.gatech.edu>
2022-04-22 13:13:30 -0400

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setup(
     name="ipypdf",
-    version="0.1.4",
+    version="0.1.5",
     description="Jupyter widget for applying nlp to pdf documents",
     long_description=long_description,
     long_description_content_type="text/markdown",
